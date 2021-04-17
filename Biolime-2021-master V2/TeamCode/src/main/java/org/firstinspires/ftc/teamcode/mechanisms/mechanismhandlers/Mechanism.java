8ee5663debc15463033bc7db4a00637ba5cf2c81
RandyNgo02 <35178937+RandyNgo02@users.noreply.github.com>
2021-04-17 14:02:10 -0600

    Deleted

--- Biolime-2021-master V2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/mechanismhandlers/Mechanism.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.mechanisms.mechanismhandlers;
-
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-public abstract class Mechanism {
-    public abstract void init(HardwareMap hwmap);
-}
